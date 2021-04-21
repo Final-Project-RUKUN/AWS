99f5a8c146cb5317b38ba49c02246fd7e08608c5
caturprasatya <caturprasatya1224@gmail.com>
2021-04-21 11:24:33 +0700

    add midtrans in server aws

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,28 @@
 const route = require('express').Router()
 const userRoute = require('./userRoute')
+const adminRoute = require('./adminRoute')
 const villageRoute = require('./villageRoute')
+const villagersRoute = require('./villagersRoute')
 const transactionRoute = require('./transactionRoute')
 const suggestionRoute = require('./suggestioRoute')
-const adminRoute = require('./adminRoute')
+const midtransRoute = require('./midtransRoute')
 const { authenticate } = require('../middlewares/auth')
+
+
+route.get('/' ,(req, res)=>{
+  res.send('gooooo')
+})
 
 route.use('/user', userRoute)
 
 route.use('/admin', adminRoute)
 
 route.use(authenticate)
+route.use('/midtrans', midtransRoute)
 
-route.use('/villagers', villageRoute)
+route.use('/village', villageRoute)
+
+route.use('/villagers', villagersRoute)
 
 route.use('/transactions', transactionRoute)
 
